f135ae9cb6d94b2e7c1bb329e812ab0e6c050a70
vbzt <vitorcastrobuzato@gmail.com>
2026-01-25 08:21:32 -0300

    feat: add Prisma, Auth, and User modules to AppModule; implement global validation pipe in main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
