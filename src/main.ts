c93c5070fe767c124dc003347299b021a3a8f115
vbzt <vitorcastrobuzato@gmail.com>
2026-03-10 22:16:28 -0300

    feat(app) add cors and helmet to security layer

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
+  app.use(helmet())
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe( { whitelist: true, forbidNonWhitelisted: true, transform: true}))
   await app.listen(process.env.PORT ?? 3000);
 }
