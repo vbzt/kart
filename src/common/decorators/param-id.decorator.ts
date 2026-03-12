19e2a576996efe9aa54613255f392bbbcd28354c
vbzt <vitorcastrobuzato@gmail.com>
2026-03-12 19:37:24 -0300

    refactor(decorator): enhance ParamId decorator to validate UUID v4 format

--- src/common/decorators/param-id.decorator.ts
+++ src/common/decorators/param-id.decorator.ts
@@ -1,7 +1,19 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { BadRequestException, createParamDecorator, ExecutionContext } from "@nestjs/common";
 
-export const ParamId = createParamDecorator(( args: string | undefined, context: ExecutionContext) => {
-  const req = context.switchToHttp().getRequest()
-  const id = req.params.id
-  return id 
+export const ParamId = createParamDecorator((data: unknown, context: ExecutionContext) => {
+  const req = context.switchToHttp().getRequest();
+  const id = req.params.id as string | undefined;
+
+  if (!id) {
+    throw new BadRequestException('Parâmetro "id" é obrigatório.');
+  }
+
+  const uuidV4Regex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+  if (!uuidV4Regex.test(id)) {
+    throw new BadRequestException('Parâmetro "id" deve ser um UUID v4 válido.');
+  }
+
+  return id;
 })
