553333dfddd67dda791cea2bfaa3abb1262c86f6
vbzt <vitorcastrobuzato@gmail.com>
2026-03-12 19:40:11 -0300

    refactor(subscription): improve SubscriptionGuard implementation with better readability and structure

--- src/common/guards/subscription.guard.ts
+++ src/common/guards/subscription.guard.ts
@@ -1,17 +1,21 @@
-import { BadRequestException, CanActivate, ExecutionContext } from "@nestjs/common";
-import { SubscriptionService } from "src/modules/subscription/subscription.service";
+import { BadRequestException, CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { SubscriptionService } from 'src/modules/subscription/subscription.service';
 
-export class SubscriptionGuard implements CanActivate{
-  constructor(private readonly subscriptionService: SubscriptionService){}
-  async canActivate(context: ExecutionContext):Promise<boolean> {
-    const req = context.switchToHttp().getRequest()
-    const user = req.user
-    const {subscription, hasActiveSubscription} = await this.subscriptionService.readActiveSubscription(user.userId)
-    if(!hasActiveSubscription) return false
+@Injectable()
+export class SubscriptionGuard implements CanActivate {
+  constructor(private readonly subscriptionService: SubscriptionService) {}
 
-    req.subscription = subscription
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const user = req.user;
+    const { subscription, hasActiveSubscription } =
+      await this.subscriptionService.readActiveSubscription(user.userId);
 
-    return true
-    
+    if (!hasActiveSubscription) {
+      return false;
+    }
+
+    req.subscription = subscription;
+    return true;
   }
 }
